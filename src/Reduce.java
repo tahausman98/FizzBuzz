10e1b5f18b73495c7d63fd0a1782b3a5b10508cc
taha <tahausman2303@gmail.com>
2023-09-12 08:37:34 -0400

    update

---
+++ src/Reduce.java
@@ -0,0 +1,15 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int i = 100;
+        while (i != 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+            } else {
+                i -= 1;
+            }
+            steps += 1;
+        }
+        System.out.println(steps);
+    }
+}
